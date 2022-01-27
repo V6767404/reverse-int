2eabf7afd5f9a5c91dda3db66d13af6aa4fa5d3a
V6767404 <32267531+V6767404@users.noreply.github.com>
2022-01-27 20:21:29 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
+    
+    result = 0;
+    n = Math.abs(n);
+    
+    while (n !== 0) {
+
+        result *= 10;
+        result += n % 10;
+
+        n -= n % 10;
+        n /= 10;
+    }
+    return result;
   
 }
