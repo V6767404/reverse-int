13716772b4cbb27a792e8189b21927922e26b085
V6767404 <32267531+V6767404@users.noreply.github.com>
2022-02-02 17:04:39 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -24,3 +24,5 @@ return +Math.abs(n)
 .reverse()
 .join('');
 */
+// Three Ways to Reverse a String in JavaScript
+// https://www.freecodecamp.org/news/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb/
